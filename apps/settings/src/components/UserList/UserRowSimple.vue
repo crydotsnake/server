46f96d777350bfb8a2b516f68ba3419d3a3e80de
GretaD <gretadoci@gmail.com>
2020-04-23 21:33:56 +0200

    fixup! fixup! fixup! Fix the keyboard tab for actionmenu
    Signed-off-by: GretaD <gretadoci@gmail.com>

--- apps/settings/src/components/UserList/UserRowSimple.vue
+++ apps/settings/src/components/UserList/UserRowSimple.vue
@@ -174,10 +174,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 	.cellText {
 		overflow: hidden;
 		text-overflow: ellipsis;
 		white-space: nowrap;
+}
+	.icon-more {
+		background-color: var(--color-main-background);
+		background-image: var(--icon-more-000);
+		border: 0;
 	}
 </style>
